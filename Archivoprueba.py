932743aa7ae024b622645147df9057ac162ea7f2
Nmaroni22 <niicomaronienespania@gmail.com>
2025-05-09 23:46:50 +0200

    Segundo commit

--- Archivoprueba.py
+++ Archivoprueba.py
@@ -6,3 +6,9 @@ print("Comprobando diferencias entre editar archivos entre distintas 'ramas'")
 print("Joan es un tipazo.")
 print("No veo la diferencia en los iconos")
 
+print("hola")
+print("Probando un segundo commit")
+
+class Messi(10):
+    pass
+
